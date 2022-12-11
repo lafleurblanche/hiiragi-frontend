17d4b3304d124cb312bddd99ef5df114057e8b59
lafleurblanche <aquamint@squall.dti.ne.jp>
2022-12-11 21:47:52 +0900

    20221211 NEW

---
+++ hiiragi-frontend/src/components/ui-elements/Link/ListItemsLink.ts
@@ -0,0 +1,8 @@
+/* mui5 */
+import Link from "@mui/material/Link";
+import { styled } from "@mui/material/styles";
+
+export const ListItemsLink = styled(Link)({
+  textDecoration: "none",
+  color: "inherit"
+});
